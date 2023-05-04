fadbec290ea4fae36684eb8982ffb344bb11cc96
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2023-05-04 14:16:43 -0300

    calculando os valores do campo resultado e colocando novamente no campo resultado.

--- script.js
+++ script.js
@@ -24,7 +24,17 @@ function calculadora() {
     }
   };
 
+  const setResult = (event) => {
+    const valuesKeys = event.target.dataset.teclas;
+    const resultValues = result.innerText;
+    resultValues.split("''").join("");
+    if (valuesKeys === "igual" && result.innerHTML !== "") {
+      result.innerHTML = eval(resultValues);
+    }
+  };
+
   _keys.forEach((item) => item.addEventListener("click", setValues));
   _keys.forEach((item) => item.addEventListener("click", resetValues));
+  _keys.forEach((item) => item.addEventListener("click", setResult));
 }
 calculadora();
